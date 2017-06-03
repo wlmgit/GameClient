7e72aad4fe22364a325b9fa997b8dadcbdc49c8c
wlmgit <1621020745@qq.com>
2017-06-03 16:15:00 +0800

    First Commit

---
+++ GameClient/Assets/Scripts/Config/ItemConfig.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemConfig {
+
+    public string name;
+    public string itemId;
+}
---
+++ GameClient/Assets/Scripts/Config/ItemConfigManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemConfigManager{
+
+    public List<ItemConfig> cfgList;
+
+    public ItemConfigManager()
+    {
+        cfgList = new List<ItemConfig>();
+    }
+
+    private static ItemConfigManager _instance;
+
+    public static ItemConfigManager instance()
+    {
+        if (_instance == null)
+            _instance = new ItemConfigManager();
+        return _instance;
+    }
+
+    public void setup(Object obj)
+    {
+        ItemConfig cfhInfo;
+        foreach (object cfg in obj)
+        {
+
+        }
+    }
+}
