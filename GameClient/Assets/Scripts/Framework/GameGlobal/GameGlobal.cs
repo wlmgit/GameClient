5acac0e0edc8f0954d76622daa0ecc281f3e5533
wlmgit <1621020745@qq.com>
2018-08-10 11:47:08 +0800

    delete

---
+++ GameClient/Assets/Scripts/Framework/GameGlobal/GameGlobal.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace UnityFramework
+{
+    /// <summary>
+    /// unity 组件类，避免其他类继承mono，使用此类执行
+    /// </summary>
+    public class GameGlobal : MonoBehaviour
+    {
+        private static GameGlobal instance;
+
+        public static GameGlobal Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    GameObject gameGlobal = new GameObject();
+                    gameGlobal.name = "gameGlobal";
+                    DontDestroyOnLoad(gameGlobal);
+                    instance = gameGlobal.AddComponent<GameGlobal>();
+                }
+                return instance;
+            }
+        }
+
+
+
+        #region unity Function
+        // Use this for initialization
+        void Start()
+        {
+            LoadScene();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            
+        }
+        #endregion
+
+        /// <summary>
+        /// 处理协成
+        /// </summary>
+        /// <param name="coroutine"></param>
+        public void DoCorountine(IEnumerator coroutine)
+        {
+            StartCoroutine(coroutine);
+        }
+
+        public async void LoadScene()
+        {
+            Debug.Log("加载前");
+
+            await this.ChangeScene();
+
+            Debug.Log("after load");
+
+        }
+
+        public async Task ChangeScene()
+        {
+            SceneManager.LoadScene("test");
+        }
+    }
+}
+
+
